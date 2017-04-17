af2ab6d540c0360ed5dec05177e1c2d9bac49258
Michael Nguyen <michael.nguyen@alumni.ubc.ca>
2017-04-16 22:52:12 -0700

    save pdf, need to allow client to download pdf still

--- app/routes/shipments.js
+++ app/routes/shipments.js
@@ -1,5 +1,10 @@
 
 const express = require('express');
+const js2xmlparser = require("js2xmlparser");
+const xml2js = require('xml2js');
+const _ = require('lodash');
+const request = require('request');
+const fs = require('fs');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
@@ -10,7 +15,72 @@ router.get('/', (req, res, next) => {
 
 router.post('/sendInformation', (req, res) => {
 
-	res.status(200).send(req.body)
+	req.body["@"] = {
+		"xmlns": "http://www.canadapost.ca/ws/ncshipment-v4"
+	}
+
+	if (req.body["delivery-spec"]["preferences"]) {
+		req.body["delivery-spec"]["preferences"]["show-packing-instructions"] = true;
+	} else {
+		req.body["delivery-spec"]["preferences"] = {
+			"show-packing-instructions": false
+		}
+	}
+
+	var xml = js2xmlparser.parse("non-contract-shipment", req.body, {"declaration": {"encoding": "UTF-8"}})
+
+	request(
+	{
+		url: "https://ct.soa-gw.canadapost.ca/rs/0008545499/ncshipment",
+		method: "POST",
+		headers: {
+			"Accept": "application/vnd.cpc.ncshipment-v4+xml",
+			"Content-Type": "application/vnd.cpc.ncshipment-v4+xml",
+			"Authorization": "Basic NmY0OWVhOTAxNjE3NDFiMTpmNmU4MDY3ZWNlMzUyYWIwYWU3Mzg2",
+			"Accept-language": "en-CA"
+		},
+		body: xml
+	}, function(error, response, body) {
+
+		if (error) {
+			res.status(400).send('Bad Request');
+		}
+
+		var parser = new xml2js.Parser();
+
+		parser.parseString(body, function(err, result) {
+			var json = JSON.stringify(result);
+			json = JSON.parse(json);
+
+			var links = json["non-contract-shipment-info"]["links"][0]["link"]
+
+			var obj = _.find(links, function(el) {
+				return _.get(el, '$.rel') === 'label';
+			});
+
+			var url = _.get(obj, '$.href');
+			console.log(url);
+
+			request(
+			{
+				url: url,
+				method: "GET",
+				headers: {
+					"Accept": "application/pdf",
+					"Authorization": "Basic NmY0OWVhOTAxNjE3NDFiMTpmNmU4MDY3ZWNlMzUyYWIwYWU3Mzg2",
+					"Accept-language": "en-CA"
+				},
+				encoding: 'binary'
+			}, function (error, response, body) {
+
+				fs.writeFileSync("label.pdf", body, 'binary');
+
+			});
+
+		});
+	});
+
+	
 
 })
 
